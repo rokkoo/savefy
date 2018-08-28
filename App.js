9d712cd6d8042ce249cd65f14967059b98aaeef3
Alfonso <alfonsolasaguirre@gmail.com>
2018-08-28 12:53:04 +0200

    Calendario añadido, selecciona el dia actual y el dia leccionado.

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { LoginNavigator } from "./navigator";
+import { LocaleConfig } from "react-native-calendars";
 
 export default class App extends React.Component {
   render() {
